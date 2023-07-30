17fbbda9d515d3ddc72c0c430e9107fc3b91e0fa
Strizhak Roman <milon777.official@gmail.com>
2023-07-30 12:49:22 +0300

    Added loadingSpinner and errorMessage components

--- src/components/randomCharacter/RandomCharacter.tsx
+++ src/components/randomCharacter/RandomCharacter.tsx
@@ -3,10 +3,14 @@ import "./randomCharacter.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 import MarvelService from "../../services/MarvelService";
 import { CharacterInfo } from "../../services/ResponseInterfaces";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 interface Character extends Record<keyof CharacterInfo, null | string> {}
 interface State {
     character: Character;
+    loading: boolean;
+    error: boolean;
 }
 interface Props {}
 
@@ -24,55 +28,39 @@ class RandomCharacter extends Component<Props, State> {
             homepage: null,
             wiki: null,
         },
+        loading: true,
+        error: false,
     };
 
     marvelService = new MarvelService();
 
     onCharacterLoaded = (character: Character): void => {
-        this.setState({ character });
+        this.setState({ character, loading: false });
+    };
+
+    onError = (): void => {
+        this.setState({ loading: false, error: true });
     };
 
     updateCharacter = (): void => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        this.marvelService.getCharacterById(id).then(this.onCharacterLoaded);
+        this.marvelService
+            .getCharacterById(id)
+            .then(this.onCharacterLoaded)
+            .catch(this.onError);
     };
 
     render() {
-        const {
-            character: { name, description, thumbnail, homepage, wiki },
-        } = this.state;
-
-        console.log(`descr: ${description}`);
+        const { character, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const loadingSpinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View {...character} /> : null;
 
         return (
             <div className="random-character">
-                <div className="random-character__block">
-                    <img
-                        src={thumbnail ?? "#"}
-                        alt="Random character"
-                        className="random-character__img"
-                    />
-                    <div className="random-character__info">
-                        <p className="random-character__name">{name}</p>
-                        <p className="random-character__descr">
-                            {description ?? "No description"}
-                        </p>
-                        <div className="random-character__btns">
-                            <a
-                                href={homepage ?? "#"}
-                                className="button button__main"
-                            >
-                                <div className="inner">Homepage</div>
-                            </a>
-                            <a
-                                href={wiki ?? "#"}
-                                className="button button__secondary"
-                            >
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {loadingSpinner}
+                {content}
                 <div className="random-character__static">
                     <p className="random-character__title">
                         Random character for today!
@@ -95,5 +83,33 @@ class RandomCharacter extends Component<Props, State> {
         );
     }
 }
+
+const View: React.FC<Character> = (character) => {
+    const { name, description, thumbnail, homepage, wiki } = character;
+
+    return (
+        <div className="random-character__block">
+            <img
+                src={thumbnail ?? "#"}
+                alt="Random character"
+                className="random-character__img"
+            />
+            <div className="random-character__info">
+                <p className="random-character__name">{name}</p>
+                <p className="random-character__descr">
+                    {description ?? "No description"}
+                </p>
+                <div className="random-character__btns">
+                    <a href={homepage ?? "#"} className="button button__main">
+                        <div className="inner">Homepage</div>
+                    </a>
+                    <a href={wiki ?? "#"} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default RandomCharacter;
